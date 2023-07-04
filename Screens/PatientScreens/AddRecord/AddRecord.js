6fd26934ddfb9dd6dc5667a6a2a64978d55d3aa1
usamasaeed1028 <usamasaeed1028@gmail.com>
2023-07-04 18:04:23 +0500

    feat: multiple images added on pickImage button!

--- Screens/PatientScreens/AddRecord/AddRecord.js
+++ Screens/PatientScreens/AddRecord/AddRecord.js
@@ -1,71 +1,89 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React, { useState, useEffect } from 'react';
-import * as ImagePicker from 'expo-image-picker';
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 import { colors } from "../../../constants/colors";
 import Button from "../../../components/UI/Button";
 import PickImage from "../../../components/Records/PickImage";
 import UploadImage from "../../../components/Records/UploadImage";
 
-const AddRecord = ({ navigation,route }) => {
-    const [showPickComponent, setPickShowComponent] = useState(false);
-    //const [childComponentVisible, setChildComponentVisible] = useState(false);
+const AddRecord = ({ navigation, route }) => {
+  const [showPickComponent, setPickShowComponent] = useState(false);
+  //const [childComponentVisible, setChildComponentVisible] = useState(false);
+  const [images, setImages] = useState([]);
 
-  console.log(showPickComponent)
-    const pressHandlerPick = () => {
-      console.log("button")
-        setPickShowComponent(true);
-       // <PickImage/>
-    };
-    const pressHandlerUpload = () => {
-        navigation.navigate("UploadImage");
-       // <UploadImage/>
-    };
-    const handleChildComponentComplete = () => {
-        setPickShowComponent(false);
-        // Perform any other actions in the parent component after the child component is complete
-      };
+  console.log(showPickComponent);
+  const pressHandlerPick = () => {
+    console.log("button");
+    setPickShowComponent(true);
+    // <PickImage/>
+  };
+  const pressHandlerUpload = () => {
+    navigation.navigate("UploadImage");
+    // <UploadImage/>
+  };
+  const handleChildComponentComplete = () => {
+    setPickShowComponent(false);
+  };
+  const handleImage = (imageUrl) => {
+    setPickShowComponent(false);
+    setImages((prev) => [...prev, imageUrl]);
+  };
 
-    return (
-        <View style={styles.container} >
-            <Text style={styles.title}>RECORD</Text>
-            <View style={styles.buttonContainer}>
-              <Button onPress={pressHandlerPick} style={styles.button}>Pick Image</Button>
-              <Button onPress={pressHandlerUpload} style={styles.button}>Upload Image</Button>
-              
-            </View>
-            {showPickComponent && <PickImage showPickComponent={showPickComponent}  setPickShowComponent={setPickShowComponent} /> }
-  
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>RECORD</Text>
+        <View style={styles.buttonContainer}>
+          <Button onPress={pressHandlerPick} style={styles.button}>
+            Pick Image
+          </Button>
+          <Button onPress={pressHandlerUpload} style={styles.button}>
+            Upload Image
+          </Button>
         </View>
-      );
-}
+        {showPickComponent && <PickImage onSelectImage={handleImage} />}
+        {images.map((imageUrl) => {
+          return (
+            <View style={{margin:10}} key={imageUrl}>
+              <Image
+                source={{ uri: imageUrl }}
+                style={{ width: 200, height: 200 }}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
 
 export default AddRecord;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: colors.offWhite,
-    },
-    button: {
-        fontSize: 12,
-        marginTop:30,
-        alignItems: 'center',
-        //backgroundColor: '#006070',
-        backgroundColor: colors.blue,
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    image: {
-        flex: 1/2,
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        justifyContent: 'center',
-      },
-    title: {
-      fontSize: 24,
-      fontWeight: "bold",
-      textAlign: "center",
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: colors.offWhite,
+  },
+  button: {
+    fontSize: 12,
+    marginTop: 30,
+    alignItems: "center",
+    //backgroundColor: '#006070',
+    backgroundColor: colors.blue,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  image: {
+    flex: 1 / 2,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
--- components/Records/PickImage.js
+++ components/Records/PickImage.js
@@ -1,25 +1,22 @@
 import { Image, StyleSheet, Text, View } from "react-native";
-import React, { useState, useEffect } from 'react';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 import { colors } from "../../constants/colors";
 import Button from "../../components/UI/Button";
 import ImageViewer from "./ImageViewer";
 import { setStatusBarHidden } from "expo-status-bar";
 
-const PickImage = ({ showPickComponent, setPickShowComponent }) => {
-
+const PickImage = ({ onSelectImage }) => {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
-  const [image, setImage] = useState([]);
-  const [imagePicked, setImagePicked] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      setHasGalleryPermission(galleryStatus.status === 'granted');
-      pickImage()
+      const galleryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
+      pickImage();
     })();
-  }, [showPickComponent]);
-
+  }, []);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -28,33 +25,16 @@ const PickImage = ({ showPickComponent, setPickShowComponent }) => {
       aspect: [4, 3],
       quality: 1,
     });
-    // console.log(result);
-    setImagePicked(true);
 
     if (!result.canceled) {
-      setImage((prev) =>  [...prev , result.assets[0].uri]) 
+      onSelectImage(result.assets[0].uri);
     }
   };
 
-  if (imagePicked) {
-    return (
-      <View style={styles.container} >
-        image.map(() => 
-          <ImageViewer selectedImage={image} />
-
-        })
-      </View>
-    );
-  }
-  if(image !== null) {
-    console.log("ssss")
-    setPickShowComponent(false)
-  }
- 
   if (hasGalleryPermission === false) {
     return <Text>No access to Gallery</Text>;
   }
-}
+};
 
 export default PickImage;
 
